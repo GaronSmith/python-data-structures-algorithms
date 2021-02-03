8627755109403387c3709ad6c114ccea3ef8afcd
GaronSmith <wgaronsmith13@gmail.com>
2021-02-03 11:26:21 -0500

    added clrs 2.1cd ..

--- reference/sorting_algorithms/insertion_sort.py
+++ reference/sorting_algorithms/insertion_sort.py
@@ -2,7 +2,7 @@ def insertion_sort(input_list):
     for i in range(1,len(input_list)):
         value = input_list[i]
         idx = i
-        while value < input_list[idx - 1] and idx>0:
+        while value > input_list[idx - 1] and idx>0: #change < to > for non increaseing 
             input_list[idx] = input_list[idx-1]
             idx -= 1
         input_list[idx] = value
