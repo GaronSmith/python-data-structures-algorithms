d6d2652b0bf68ef86176c71149fe6cd0b6168ff4
GaronSmith <wgaronsmith13@gmail.com>
2021-02-03 09:13:02 -0500

    insertion sort

---
+++ reference/sorting_algorithms/insertion_sort.py
@@ -0,0 +1,12 @@
+def insertion_sort(input_list):
+    for i in range(1,len(input_list)):
+        value = input_list[i]
+        idx = i
+        while value < input_list[idx - 1] and idx>0:
+            input_list[idx] = input_list[idx-1]
+            idx -= 1
+        input_list[idx] = value
+    
+    return input_list
+
+print(insertion_sort([1,5,3,8,6,3,9,10]))
