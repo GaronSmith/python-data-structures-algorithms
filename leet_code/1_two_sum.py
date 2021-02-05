88347b6bec6152aa73d8ef5e2f2e1dd7db78f588
GaronSmith <wgaronsmith13@gmail.com>
2021-02-05 18:45:58 -0500

    added 1_two_sum

---
+++ leet_code/1_two_sum.py
@@ -0,0 +1,10 @@
+from collections import defaultdict
+class Solution:
+    def twoSum(self, nums, target) 
+        ans = defaultdict(int)
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in ans:
+                return [ans[diff], i]
+            else:
+                ans[num] = i
