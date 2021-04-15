4a5d3b6d76ef86421bc307536fd928c31f7ee98f
GaronSmith <wgaronsmith13@gmail.com>
2021-04-15 17:30:22 -0400

    substring

---
+++ hacker_rank/get_substring_count.py
@@ -0,0 +1,12 @@
+def getSubstringCount(s):
+    # Write your code here
+    groups = [1]
+    for i in range(1, len(s)):
+        if s[i-1] != s[i]:
+            groups.append(1)
+        else:
+            groups[-1] += 1
+    ans = 0
+    for i in range(1, len(groups)):
+        ans += min(groups[i-1], groups[i])
+    return ans
