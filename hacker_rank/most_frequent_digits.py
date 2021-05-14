b7d37ff3de38cee831a9b89d1f819cd5c48158e4
GaronSmith <wgaronsmith13@gmail.com>
2021-05-14 13:49:55 +0100

    most frrequent

---
+++ hacker_rank/most_frequent_digits.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+
+
+def mostFrequentDigits(a):
+    ref = defaultdict(int)
+    maxi = float("-inf")
+    for num in a:
+        if num < 10:
+            ref[num] += 1
+            maxi = max(maxi, ref[num])
+        else:
+            ten = num // 10
+            left = num % 10
+            ref[ten] += 1
+            ref[left] += 1
+            maxi = max(maxi, ref[ten], ref[left])
+    ans = []
+    for key in ref:
+        if ref[key] == maxi:
+            ans.append(key)
+
+    return sorted(ans)
