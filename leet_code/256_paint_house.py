0d43e71b9af5c48f480dc2e77ee31ab5ccbe4fa6
GaronSmith <wgaronsmith13@gmail.com>
2021-06-01 10:34:54 +0100

    256

---
+++ leet_code/256_paint_house.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minCost(self, costs: List[List[int]]) -> int:
+        for n in reversed(range(len(costs) - 1)):
+            # Total cost of painting nth house red.
+            costs[n][0] += min(costs[n + 1][1], costs[n + 1][2])
+            # Total cost of painting nth house green.
+            costs[n][1] += min(costs[n + 1][0], costs[n + 1][2])
+            # Total cost of painting nth house blue.
+            costs[n][2] += min(costs[n + 1][0], costs[n + 1][1])
+
+        if len(costs) == 0:
+            return 0
+        return min(costs[0])  # Return the minimum in the first row.
